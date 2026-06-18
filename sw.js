b144230a3de95ab961ce51c04b27fae361796aa4
tcxbb <tcxbb@users.noreply.github.com>
2026-06-18 16:09:20 +0800

    v0.6.5 手机端排版修复

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE_NAME = "dotlog-v1";
+const CACHE_NAME = "dotlog-v0.6.5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
