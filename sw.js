34ef612156f0e2636c187028e491b16cdeca1b82
tcxbb <tcxbb@users.noreply.github.com>
2026-06-18 18:26:28 +0800

    Fix iOS calendar menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.6.6";
+const CACHE_NAME = "dotlog-v0.6.6-calendar-menu-fix";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
