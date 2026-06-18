b97c99684a77ff7fd3e0423beec7b0d3ea08f375
tcxbb <tcxbb@users.noreply.github.com>
2026-06-18 23:52:50 +0800

    Update app logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.6.7-sync-safety";
+const CACHE_NAME = "dotlog-v0.6.8-clock-logo";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
