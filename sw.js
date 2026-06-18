ffa945549a974b0a37af6905718c5759cb55d2d3
tcxbb <tcxbb@users.noreply.github.com>
2026-06-18 22:10:38 +0800

    Prevent empty Gist sync overwrite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.6.7";
+const CACHE_NAME = "dotlog-v0.6.7-sync-safety";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
