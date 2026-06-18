eafe28d4551a71e4f3c8acda5fc8c16cff903936
tcxbb <tcxbb@users.noreply.github.com>
2026-06-18 21:17:11 +0800

    Improve mobile input layout for v0.6.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.6.6-reimport-fix";
+const CACHE_NAME = "dotlog-v0.6.7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
