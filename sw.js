45206c71b4dec3b90d398a2f6b6e8f9ad77d7a82
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 11:48:27 +0800

    Improve mobile record interactions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.6.9-borderless-logo";
+const CACHE_NAME = "dotlog-v0.7.0-mobile-ux";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
