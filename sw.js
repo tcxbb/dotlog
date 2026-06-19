d192b3133f7db6bc66c871bbcd2a7f55fdfddb99
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 12:32:17 +0800

    Fix classification selection flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.0-mobile-ux";
+const CACHE_NAME = "dotlog-v0.7.1-classification-flow";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
