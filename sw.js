890bcb661532ca2da003bdd762a5db339003f4c6
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 17:18:40 +0800

    Fix long press context menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.4-context-menu";
+const CACHE_NAME = "dotlog-v0.7.4.1-context-menu-fix";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
