b6f439ce093fef55943ac106985dace9580afacd
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 16:49:32 +0800

    Replace swipe actions with context menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.3-record-redesign";
+const CACHE_NAME = "dotlog-v0.7.4-context-menu";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
