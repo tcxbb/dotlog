8216d6b69c9976945f281122170290389a88a611
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 21:39:03 +0800

    Release Dotlog v0.7.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.6-ui-polish";
+const CACHE_NAME = "dotlog-v0.7.7-settings-theme-menu";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
