9b9bf891211b40038df439bb0c4851c81711e5c9
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 22:49:38 +0800

    Hide FAB when input sheet is open

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.10-sheet-date-overlay-tag-confirm";
+const CACHE_NAME = "dotlog-v0.7.10-fab-hidden-week-divider";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
