f008e43b6708c618e36674c6536fc4622cc5b000
tcxbb <tcxbb@users.noreply.github.com>
2026-06-19 22:35:15 +0800

    Release Dotlog v0.7.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.9-category-colors-confirm-overlay";
+const CACHE_NAME = "dotlog-v0.7.10-sheet-date-overlay-tag-confirm";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
