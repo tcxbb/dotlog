05f2af58f6c008e46032aaee960473a5b76f01be
tcxbb <tcxbb@users.noreply.github.com>
2026-06-20 18:40:38 +0800

    新增标签触发词编辑面板

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.2.4-collapsible-tag-settings";
+const CACHE_NAME = "dotlog-v1.2.5-tag-trigger-editor";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
