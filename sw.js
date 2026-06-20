9cf8e358cbe7dafc4bde3a7d588aeb1ae8d926db
tcxbb <tcxbb@users.noreply.github.com>
2026-06-20 17:55:03 +0800

    优化分类与标签管理列表布局

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.2.2-sheet-overlay-layer";
+const CACHE_NAME = "dotlog-v1.2.3-grouped-tag-settings";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
