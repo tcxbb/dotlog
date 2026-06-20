3452f7d4af68116b9fc0cae14a4e9c78f43a4113
tcxbb <tcxbb@users.noreply.github.com>
2026-06-20 14:02:41 +0800

    优化时间面板对齐与补录结束时间

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v0.7.10-compact-time-controls";
+const CACHE_NAME = "dotlog-v0.7.10-aligned-time-panels";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
