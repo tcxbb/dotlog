045f7e314cc7721cddaaaa5a39f1cf5f44ad4229
tcxbb <tcxbb@users.noreply.github.com>
2026-06-21 11:26:58 +0800

    v1.3.4 add date page navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.3.3-week-swipe-single-settle";
+const CACHE_NAME = "dotlog-v1.3.4-date-page-nav-today-swipe";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
