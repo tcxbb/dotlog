81b518c2aadb9db619d8eead29a4f51019a0e7b5
tcxbb <tcxbb@users.noreply.github.com>
2026-06-22 11:44:04 +0800

    v1.4.15 add record creation and update timestamps

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.4.14";
+const CACHE_NAME = "dotlog-v1.4.15";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
