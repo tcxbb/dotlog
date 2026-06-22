9987161651fdf9438fab20df697039d90f5b0e47
tcxbb <tcxbb@users.noreply.github.com>
2026-06-22 12:00:27 +0800

    v1.4.17 allow confirmed mobile sparse union sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.4.16";
+const CACHE_NAME = "dotlog-v1.4.17";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
