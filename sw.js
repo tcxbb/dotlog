ffbf1fcc1756c1153c8755f46fe1a4f459459e4d
tcxbb <tcxbb@users.noreply.github.com>
2026-06-22 16:50:26 +0800

    修复时间格式升级导致的同步冲突

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.5.0";
+const CACHE_NAME = "dotlog-v1.5.1";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
