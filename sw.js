207da87a9ba1981a17d5dfcc99bf195f24f836ad
tcxbb <tcxbb@users.noreply.github.com>
2026-08-28 18:37:37 +0800

    限制手动同步备份保留数量

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dotlog-v1.6.9";
+const CACHE_NAME = "dotlog-v1.6.10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./dotlog-logo-final.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
